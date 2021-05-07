b0ac134919047c2647d2a045a1f73242f5cc149c
Jamie STevens <jamiestevens2000@gmail.com>
2021-05-07 12:29:40 +0100

    Sense hat test

--- device/test.py
+++ device/test.py
@@ -1,4 +1,7 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
-sense.clear((43,16,19))
+blue = (0, 0, 255)
+yellow = (255,255,0)
+
+sense.show_message("Little Bug", text_colour=yellow, back_colour=blue)
