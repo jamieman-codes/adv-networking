0d82e564cd37452d5de3fde66ed17cb9d3a850d0
Jamie STevens <jamiestevens2000@gmail.com>
2021-05-10 18:02:38 +0100

    Pubsub stuff. Flask init. App engine deploy

--- app-engine/devices.py
+++ app-engine/devices.py
@@ -1,5 +1,6 @@
 from google.cloud import iot_v1
 from google.oauth2 import service_account
+from google.cloud import pubsub_v1
 
 PROJECT_ID = "advnetworking"
 CLOUD_REGION = "europe-west1"
@@ -13,6 +14,29 @@ def updateDevice(deviceID, data):
         request={"name": device_path, "binary_data": data}
     )
 
+def sensorCallback(message):
+    data = message.data.decode("utf-8")
+    if data == "Sensor is offline" or data == "Sensor is online":
+        print(data)
+    else:
+        data = data.split(",")
+        print('Temp: {}, Hum: {}, Pressure: {}'.format(data[0], data[1], data[2]))
+    message.ack()
 
+def pullSub(subID, callback):
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(PROJECT_ID, subID)
+
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    print(f"Listening for messages on {subscription_path}..\n")
+    # Wrap subscriber in a 'with' block to automatically call close() when done.
+    with subscriber:
+        try:
+            # When `timeout` is not set, result() will block indefinitely,
+            # unless an exception is encountered first.
+            streaming_pull_future.result(timeout=5.0)
+        except TimeoutError:
+            streaming_pull_future.cancel()
 
 #updateDevice("led-matrix", "ON BLUE")
+pullSub("sensors", sensorCallback)
